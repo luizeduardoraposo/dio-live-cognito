2694cd36a9f932da30804e37964e2b3ba40c1925
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2023-05-30 22:31:32 -0300

    add update function

---
+++ src/update_item_function.js
@@ -0,0 +1,24 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const main = async () => {
+  const command = new UpdateCommand({
+    TableName: "Dogs",
+    Key: {
+      Breed: "Labrador",
+    },
+    UpdateExpression: "set Color = :color",
+    ExpressionAttributeValues: {
+      ":color": "black",
+    },
+    ReturnValues: "ALL_NEW",
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+};
+
